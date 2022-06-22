24efae2a31fc6519bf0a76cb4dcfdd3f42b06d36
Matthias Leitl <a.dead.trousers@gmail.com>
2022-06-22 20:52:14 +0200

    Finally bootable

---
+++ deviceCommon.mk
@@ -0,0 +1,17 @@
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# If you are building from TWRP's minimal source, Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+#$(call inherit-product, vendor/twrp/config/gsm.mk)
+
+# Another common config inclusion
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# Files under $(LOCAL_PATH)/recovery/root/ get automatically copied into recovery
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/Unihertz/Atom_LXL/recovery/root,recovery/root)
