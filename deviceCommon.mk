4d8d791de951528c0b7ebd1bf0f9d14364ea1d61
Matthias Leitl <a.dead.trousers@gmail.com>
2022-12-16 21:07:39 +0100

    Reduce the size of the ramdisk to be able to boot

--- deviceCommon.mk
+++ deviceCommon.mk
@@ -1,9 +1,10 @@
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # If you are building from TWRP's minimal source, Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
