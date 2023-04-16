861a24e5277115f1d03f0859de323a50a7d043bc
Matthias Leitl <a.dead.trousers@gmail.com>
2023-04-16 14:00:35 +0200

    Merge branch 'twrp-11.0-crypto'

--- deviceCommon.mk
+++ deviceCommon.mk
@@ -1,9 +1,10 @@
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # If you are building from TWRP's minimal source, Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -11,3 +12,5 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Another common config inclusion
 #$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+PRODUCT_SHIPPING_API_LEVEL := 30
